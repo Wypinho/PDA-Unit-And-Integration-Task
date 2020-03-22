3ecef2e3d6b06bbd574fb49631cf0f73be89421c
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-03-22 16:43:38 +0000

    integration test 5 passed

--- tests/integration/tests.js
+++ tests/integration/tests.js
@@ -25,4 +25,32 @@ describe('calculator functionality', function() {
     expect(running_total.getAttribute('value')).to.eventually.equal('4')
   })
 
+  it('can chain multiple operations', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#operator_add')).click();
+    element(by.css('#number2')).click();
+    element(by.css('#operator_multiply')).click();
+    element(by.css('#number3')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('12')
+  })
+
+  it('displays positive numbers as expected', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number2')).click();
+    element(by.css('#number2')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('22')
+  })
+
+  it('displays negative numbers as expected', function(){
+    running_total = element(by.css('#running_total'))
+    element(by.css('#number5')).click();
+    element(by.css('#operator_subtract')).click();
+    element(by.css('#number1')).click();
+    element(by.css('#number0')).click();
+    element(by.css('#operator_equals')).click();
+    expect(running_total.getAttribute('value')).to.eventually.equal('-5')
+  })
+
 });
