b5b251f02ecefeb13807f95e61dc9817334fd6ae
Andrew Wyper <andy_wyper@yahoo.co.uk>
2020-03-22 16:00:05 +0000

    unit tests complete

--- tests/unit/calculator_spec.js
+++ tests/unit/calculator_spec.js
@@ -53,20 +53,23 @@ describe('calculator', function () {
 
   it('can chain operations together', function(){
     calculator.numberClick(2)
-    // console.log('Previous: ', calculator.previousTotal);
-    // console.log('Current: ', calculator.runningTotal);
     calculator.operatorClick('*')
     calculator.numberClick(2)
-    // console.log('Previous: ', calculator.previousTotal);
-    // console.log('Current: ', calculator.runningTotal);
     calculator.operatorClick('+')
-    // console.log('Previous: ', calculator.previousTotal);
-    // console.log('Current: ', calculator.runningTotal);
     calculator.numberClick(5)
-    // console.log('Previous: ', calculator.previousTotal);
-    // console.log('Current: ', calculator.runningTotal);
     calculator.operatorClick('=')
     assert.equal(calculator.runningTotal, 9)
+  })
+
+  it('clear doesnt affect calculation', function(){
+    calculator.numberClick(6)
+    calculator.operatorClick('+')
+    calculator.numberClick(6)
+    calculator.operatorClick('+')
+    calculator.clearClick()
+    calculator.numberClick(6)
+    calculator.operatorClick('=')
+    assert.equal(calculator.runningTotal, 18)
   })
 
 });
